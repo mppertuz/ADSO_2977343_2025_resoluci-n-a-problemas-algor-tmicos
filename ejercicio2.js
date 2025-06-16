28550799d0a841e931eb1d6d3a85e0057cb7e773
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-06-16 10:40:53 -0500

    cambioUbicacion

---
+++ ejercicio2.js
@@ -0,0 +1,19 @@
+let edades = [];
+
+let menores = 0;
+let mayores = 0;
+let adultoMayor = 0;
+let promedioEdades = 0;
+
+for (let i = 0; i < 10; i++) {
+    let edad; 
+
+    do {
+        edad = parseInt(prompt("Ingrese la edad de la persona ", + {i + 1} (entre 1 y 120 años), ":",ADSFJKLÑ}));
+        if (isNaN(edad) || edad < 0 || edad > 120) {
+            alert("Por favor, ingrese una edad válida entre 0 y 120.");
+        }
+    }
+
+
+}
