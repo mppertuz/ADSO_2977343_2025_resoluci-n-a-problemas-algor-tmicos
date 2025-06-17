f7668a8071b156e2993acbe4353a4e25f3272810
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-06-16 21:08:13 -0500

    arregloVectores

---
+++ ejercicio3.js
@@ -0,0 +1,59 @@
+
+// Declarar los vectores
+let vector1 = [];
+let vector2 = [];
+
+// Función para ingresar datos en orden ascendente
+function ingresarvector(nombreVector) {
+
+let vector = [];
+    for (let i = 0; i < 5; i++) {
+        let numero = parseInt(prompt(`Ingrese el número #${i + 1} para ${nombreVector}:`));
+
+        // Validar número y orden ascendente
+        while (
+                isNaN(numero) || 
+                (i > 0 && numero < vector[i - 1])
+        ) {
+            alert(`Error: Debe ingresar un número mayor o igual a ${vector[i - 1] ?? 'cero'} y válido.`);
+            numero = parseInt(prompt(`Ingrese nuevamente el número #${i + 1} para ${nombreVector}:`));
+        } 
+        vector [i] = numero;
+    }
+    return vector;
+}
+
+// Ingreso de datos
+vector1 = ingresarvector("Vector 1");
+vector2 = ingresarvector("Vector 2");   
+
+// Mezclar los dos vectores
+let Mezcla = []
+let i = 0, j = 0;
+
+// Comparar y mezclar
+while (i < vector1.length && j < vector2.length) {
+    if (vector1[i] < vector2[j]) {
+        Mezcla.push(vector1[i]);
+        i++;
+    } else {
+        Mezcla.push(vector2[j]);
+        j++;
+    }
+}
+
+// Agregar lo que falte de cada vector
+
+while (i < vector1.length) {
+    Mezcla.push(vector1[i]);
+    i++;
+}
+while (j < vector2.length) {
+    Mezcla.push(vector2[j]);
+    j++;
+}
+
+// Mostrar el resultado
+console.log(ADSFJKLÑ});
+
+
